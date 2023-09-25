545aa93fa7c616e4821af906355b1d69509b53bc
nishiyama <nishiyama@tdsl.co.jp>
2023-09-25 12:10:32 +0900

    テストページの追加とかクラスパスの変更とか

---
+++ src/main/java/action/TestAction.java
@@ -0,0 +1,44 @@
+package action;
+
+import java.io.IOException;
+import constents.Const.Path;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestAction
+ */
+public class TestAction extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Default constructor. 
+	 */
+	public TestAction() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+        RequestDispatcher dispatcher = request
+                .getRequestDispatcher("/test.jsp");
+        dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,20 +1,31 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Hello</title>
+<title>メインページ</title>
+<%@ include file="../header.jsp"%>
 </head>
 <body>
 
-	<form name="login" action="<%=request.getContextPath()%>/SyainJouhouAction"
-		method="post" onSubmit="return checkIDandText()">
+	<form name="login"
+		action="<%=request.getContextPath()%>/SyainJouhouAction" method="post"
+		onSubmit="return checkIDandText()">
 		<p>
 			<input type="submit" name="login" value="社員名簿の閲覧">
+		</p>
+		<p>
 			<input type="submit" name="login" value="社員名簿の閲覧（管理者用）">
 		</p>
-
+	</form>
+	
+	<form name="test"
+		action="<%=request.getContextPath()%>/TestAction" method="post"
+		onSubmit="return checkIDandText()">
+		<p>
+			<input type="submit" name="login" value="テスト用ページに遷移">
+		</p>
 	</form>
 
 </body>
