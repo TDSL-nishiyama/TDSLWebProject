b8543d147297cd9094534ec535dc8e935761633c
nishiyama <nishiyama@tdsl.co.jp>
2023-11-22 11:58:55 +0900

    NOT NULL指定の変更

--- etc/CREATE文/create_koutsuu.sql
+++ etc/CREATE文/create_koutsuu.sql
@@ -1,6 +1,6 @@
 CREATE TABLE koutsuu
-(no INT PRIMARY KEY,sendmailaddress VARCHAR(100)
+(no INT PRIMARY KEY,sendmailaddress VARCHAR(100) NOT NULL
 ,kukan1_start VARCHAR(100) NOT NULL,kukan1_end VARCHAR(100) NOT NULL,kingaku1 VARCHAR(100) NOT NULL
-,kukan2_start VARCHAR(100) NOT NULL,kukan2_end VARCHAR(100) NOT NULL,kingaku2 VARCHAR(100) NOT NULL
+,kukan2_start VARCHAR(100) ,kukan2_end VARCHAR(100),kingaku2 VARCHAR(100)
 ,bikou VARCHAR(1000)
 );
