184e4b8eb9dcf8fdbf258eaba1c605546be990ea
nishiyama <nishiyama@tdsl.co.jp>
2023-12-05 14:58:32 +0900

    バグ修正

--- src/main/java/action/ResultUserDelAction.java
+++ src/main/java/action/ResultUserDelAction.java
@@ -30,31 +30,31 @@ public class ResultUserDelAction extends HttpServlet {
     //現在ログインしているユーザーID
     HttpSession session = request.getSession();
     SessionKanriBean loginSession = (SessionKanriBean) session.getAttribute(Path.SESSION_SCOPE);
-
     int userId = loginSession.getUserId();
+    
     //画面に入力されたユーザーID
     int userIdDel = Integer.parseInt(request.getParameter("userIdDel"));
-    
+
     //共通クラスのインスタンス化
     CheckCommon checkCommon = new CheckCommon();
-    
+
     boolean errflg = false;
     UserDelBL userDelBL = new UserDelBL();
-    
+
     //入力されたIDが存在しない場合、メッセージを表示
     if (checkCommon.checkUserId(userIdDel) == 0) {
       //メッセージを格納
-      request.setAttribute(MSG.MSG_ATTRIBUTE,MSG.MASTA_DEL_1 );
+      request.setAttribute(MSG.MSG_ATTRIBUTE, MSG.MASTA_DEL_1);
       //ユーザー削除画面に遷移
       RequestDispatcher dispatcher = request
           .getRequestDispatcher(Path.USER_DEL_GAMEN);
       dispatcher.forward(request, response);
       return;
     }
-    
+
     //現在ログインしているユーザーを削除しようとした場合、メッセージを表示    
     errflg = checkCommon.checkLoginIdEqualsId(userId, userIdDel);
-    if (errflg == false) {
+    if (errflg == true) {
       //メッセージを格納
       request.setAttribute(MSG.MSG_ATTRIBUTE, MSG.MASTA_DEL_2);
       //ユーザー削除画面に遷移
@@ -62,29 +62,29 @@ public class ResultUserDelAction extends HttpServlet {
           .getRequestDispatcher(Path.USER_DEL_GAMEN);
       dispatcher.forward(request, response);
       return;
-    
-    } else {
-      //ユーザー削除を実行
-      try {
-        userDelBL.userDel(userIdDel);
-      } catch (SQLException e) {
-        //エラーメッセージを格納
-        request.setAttribute(ERRORMSG.ERRMSG_ATTRIBUTE,  ERRORMSG.DBERROR);
-        //エラー画面に遷移
-        RequestDispatcher dispatcher = request
-            .getRequestDispatcher(Path.SYSTEM_ERROR_GAMEN);
-        dispatcher.forward(request, response);
-        return;
-      }
-      //TODO ユーザー詳細TBLの退社年月日に退社日を挿入
-
-      //登録完了メッセージ
-      request.setAttribute(MSG.MSG_ATTRIBUTE, MSG.MASTA_DEL_3);
-      //ユーザー削除画面に遷移
-      RequestDispatcher dispatcher = request
-          .getRequestDispatcher(Path.USER_DEL_GAMEN);
-      dispatcher.forward(request, response);
     }
+    
+    //ユーザー削除を実行
+    try {
+      userDelBL.userDel(userIdDel);
+    } catch (SQLException e) {
+      //エラーメッセージを格納
+      request.setAttribute(ERRORMSG.ERRMSG_ATTRIBUTE, ERRORMSG.DBERROR);
+      //エラー画面に遷移
+      RequestDispatcher dispatcher = request
+          .getRequestDispatcher(Path.SYSTEM_ERROR_GAMEN);
+      dispatcher.forward(request, response);
+      return;
+    }
+    //TODO ユーザー詳細TBLの退社年月日に退社日を挿入
+
+    //登録完了メッセージ
+    request.setAttribute(MSG.MSG_ATTRIBUTE, MSG.MASTA_DEL_3);
+    //ユーザー削除画面に遷移
+    RequestDispatcher dispatcher = request
+        .getRequestDispatcher(Path.USER_DEL_GAMEN);
+    dispatcher.forward(request, response);
+
   }
 
 }
