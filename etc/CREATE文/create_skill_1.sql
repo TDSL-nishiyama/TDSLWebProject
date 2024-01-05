3dc6531771bd292c661430c8243028ea7c9f0493
nishiyama <nishiyama@tdsl.co.jp>
2024-01-05 14:47:13 +0900

    設計書の修正

---
+++ etc/CREATE文/create_skill_1.sql
@@ -0,0 +1,26 @@
+CREATE TABLE skill_1
+(
+   id INT PRIMARY KEY,
+   sikaku1 VARCHAR (100),
+   sikaku1syutokuYMD DATE,
+   sikaku2 VARCHAR (100),
+   sikaku2syutokuYMD DATE,
+   sikaku3 VARCHAR (100),
+   sikaku3syutokuYMD DATE,
+   c1kikanS DATE,
+   c1kikanE DATE,
+   carrier1 VARCHAR (1000),
+   c1position VARCHAR (100),
+   c1tech VARCHAR (1000),
+   c2kikanS DATE,
+   c2kikanE DATE,
+   carrier2 VARCHAR (1000),
+   c2position VARCHAR (100),
+   c2tech VARCHAR (1000),
+   c3kikanS DATE,
+   c3kikanE DATE,
+   carrier3 VARCHAR (1000),
+   c3position VARCHAR (100),
+   c3tech VARCHAR (1000),
+   timestamp TIMESTAMP
+);
